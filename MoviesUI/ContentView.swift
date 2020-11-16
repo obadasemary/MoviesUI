66f9653116008816ceba945be4725d7cc10f2951
Obada <obada.semary@gmail.com>
2020-11-16 21:10:44 +0300

    Initial Commit

---
+++ MoviesUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesUI
+//
+//  Created by Abdelrahman Mohamed on 16.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
