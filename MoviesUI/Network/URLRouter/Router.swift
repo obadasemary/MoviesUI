7a0c9d784c28810a5930cf7a4171c4207e06648f
Obada <obada.semary@gmail.com>
2020-11-19 04:04:25 +0300

    Update Router.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesUI/Network/URLRouter/Router.swift
+++ MoviesUI/Network/URLRouter/Router.swift
@@ -11,14 +11,33 @@
 //  https://drama-spares.com/rest-api/docs/
 import Foundation
 
+struct Constants {
+    
+    static let API_KEY = "a22cefd9"
+    
+    static let pathS = "http://www.omdbapi.com/?s="
+    static let pathI = "http://www.omdbapi.com/?i="
+    static let apiKeyString = "&apikey="
+    static let fullPath = "http://www.omdbapi.com/?s=spiderman&apikey=a22cefd9"
+    
+//    Constants.pathS + search + Constants.apiKeyString + Constants.API_KEY
+}
+
+
 struct Router: URLRouter {
     
     static var basePath: String {
-        return "https://drama-spares.com/wp-json/v1/"
+//        return "https://drama-spares.com/wp-json/v1/"
+        return "http://www.omdbapi.com"
+    }
+    
+    struct getMovieList: Readable {
+        var urlParams: String!
+        var route: String = ""
     }
  
-    struct getCountriesWithCities: Readable {
-        var urlParams: String!
-        var route: String = "cities"
-    }
+//    struct getCountriesWithCities: Readable {
+//        var urlParams: String!
+//        var route: String = "cities"
+//    }
 }
