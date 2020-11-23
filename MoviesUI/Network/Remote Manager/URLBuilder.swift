d8cf151e9447806435d17f78889cfbca9267b55b
Obada <obada.semary@gmail.com>
2020-11-23 22:23:30 +0300

    Create URLBuilder.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesUI/Network/Remote Manager/URLBuilder.swift
@@ -0,0 +1,20 @@
+//
+//  URLBuilder.swift
+//  MoviesUI
+//
+//  Created by Abdelrahman Mohamed on 23.11.2020.
+//
+
+import Foundation
+
+class URLBuilder {
+
+    enum Constants {
+        static let imageBaseUrl = "https://image.tmdb.org/t/p/w"
+    }
+
+    static func imageUrl(path: String, width: Int = 500) -> URL? {
+
+        return URL(string: "\(Constants.imageBaseUrl)\(width)\(path)")
+    }
+}
