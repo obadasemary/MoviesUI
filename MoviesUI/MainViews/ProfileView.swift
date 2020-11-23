36efbcaf4a4908433dbae2ad2c335943c27b1cb6
Obada <obada.semary@gmail.com>
2020-11-23 22:22:49 +0300

    Create ProfileView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesUI/MainViews/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  MoviesUI
+//
+//  Created by Abdelrahman Mohamed on 23.11.2020.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello, World! ProfileView")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
