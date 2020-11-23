ef2cf40e955a89657ef78ae1058737f81d4cba2a
Obada <obada.semary@gmail.com>
2020-11-23 22:22:51 +0300

    Create WishListView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesUI/MainViews/WishListView.swift
@@ -0,0 +1,20 @@
+//
+//  WishListView.swift
+//  MoviesUI
+//
+//  Created by Abdelrahman Mohamed on 23.11.2020.
+//
+
+import SwiftUI
+
+struct WishListView: View {
+    var body: some View {
+        Text("Hello, World! WishListView")
+    }
+}
+
+struct WishListView_Previews: PreviewProvider {
+    static var previews: some View {
+        WishListView()
+    }
+}
