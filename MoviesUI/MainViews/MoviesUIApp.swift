988437445d4150e42fbdd80e393c93f019b1110d
Obada <obada.semary@gmail.com>
2020-11-23 22:22:31 +0300

    Create MoviesUIApp.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesUI/MainViews/MoviesUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesUIApp.swift
+//  MoviesUI
+//
+//  Created by Abdelrahman Mohamed on 16.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct MoviesUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
