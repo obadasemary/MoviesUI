f79f8439311f22bf0825b1f38c30269e8d98d2ff
Obada <obada.semary@gmail.com>
2020-11-23 23:19:50 +0300

    Update MainServicesRemoteDS.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesUI/Network/Data Manager/MainServicesRemoteDS.swift
+++ MoviesUI/Network/Data Manager/MainServicesRemoteDS.swift
@@ -11,22 +11,30 @@ import Combine
 class MainServicesRemoteDS: MainServicesRepoType {
     
     let context: RemoteContextRequestsProtocol
+    var urlComponents = URLComponents()
 
     init(context: RemoteContextRequestsProtocol) {
         self.context = context
     }
     
     func getTopRatedMovieList() -> AnyPublisher<MovieListResponse, APIError> {
-        let params = [URLQueryItem(name: "api_key", value: apiURLConstants.apiKey)]
-        let router = Router.getTopRatedMovieList.get(params: "", httpHeaders: headers(), queryItems: params)
+        
+        let newKeyValues = [
+            "language": "en",
+            "page": "100"
+        ]
+        let parameters = defaultUrlParams.merging(newKeyValues) { (current, _) in current }
+        urlComponents.setQueryItems(with: parameters)
+
+        let router = Router.getTopRatedMovieList.get(params: "", httpHeaders: headers(), queryItems: urlComponents.queryItems)
         let publisher: AnyPublisher<MovieListResponse, APIError> = context.doRequest(request: router)
         return publisher
     }
+}
 
-//    func getCountriesWithCities() -> AnyPublisher<MovieResponse, APIError> {
-//        let params = [URLQueryItem(name: "s", value: "spiderman"), URLQueryItem(name: "apikey", value: "a22cefd9")]
-//        let router = Router.getMovieList.get(params: "", httpHeaders: headers(), queryItems: params)
-//        let publisher: AnyPublisher<MovieResponse, APIError> = context.doRequest(request: router)
-//        return publisher
-//    }
+extension URLComponents {
+    
+    mutating func setQueryItems(with parameters: [String: String]) {
+        self.queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+    }
 }
