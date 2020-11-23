7fab9a449368384e58a51516a41917f8ea4496d6
Obada <obada.semary@gmail.com>
2020-11-23 22:22:34 +0300

    Create CustomBottonTabBarTabs.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesUI/Helpers/CustomBottonTabBarTabs.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBottonTabBarTabs.swift
+//  MoviesUI
+//
+//  Created by Abdelrahman Mohamed on 23.11.2020.
+//
+
+import Foundation
+
+enum CustomBottonTabBarTabs: String {
+    
+    case movies = "film"
+    case magnifyingglass = "magnifyingglass"
+    case plus = "plus.app"
+    case heart = "heart"
+    case person = "person"
+}
