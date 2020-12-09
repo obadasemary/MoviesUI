403cdc9a54caf5acefc90f8c5a5dbf4bdf715070
Obada <obada.semary@gmail.com>
2020-12-09 05:51:33 +0300

    Update MainServicesRemoteDS.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesUI/Network/Data Manager/MainServicesRemoteDS.swift
+++ MoviesUI/Network/Data Manager/MainServicesRemoteDS.swift
@@ -17,16 +17,62 @@ class MainServicesRemoteDS: MainServicesRepoType {
         self.context = context
     }
     
-    func getTopRatedMovieList() -> AnyPublisher<MovieListResponse, APIError> {
+    func getTopRatedMovieList(page: Int) -> AnyPublisher<MovieListResponse, APIError> {
         
         let newKeyValues = [
             "language": "en",
-            "page": "100"
+            "page": String(page)
         ]
+        
         let parameters = defaultUrlParams.merging(newKeyValues) { (current, _) in current }
         urlComponents.setQueryItems(with: parameters)
 
         let router = Router.getTopRatedMovieList.get(params: "", httpHeaders: headers(), queryItems: urlComponents.queryItems)
+        let publisher: AnyPublisher<MovieListResponse, APIError> = context.doRequest(request: router)
+        return publisher
+    }
+    
+    func getUpcomingMovieList(page: Int) -> AnyPublisher<MovieListResponse, APIError> {
+        
+        let newKeyValues = [
+            "language": "en",
+            "page": String(page)
+        ]
+        
+        let parameters = defaultUrlParams.merging(newKeyValues) { (current, _) in current }
+        urlComponents.setQueryItems(with: parameters)
+
+        let router = Router.getUpcomingMovieList.get(params: "", httpHeaders: headers(), queryItems: urlComponents.queryItems)
+        let publisher: AnyPublisher<MovieListResponse, APIError> = context.doRequest(request: router)
+        return publisher
+    }
+    
+    func getNowPlayingMovieList(page: Int) -> AnyPublisher<MovieListResponse, APIError> {
+        
+        let newKeyValues = [
+            "language": "en",
+            "page": String(page)
+        ]
+        
+        let parameters = defaultUrlParams.merging(newKeyValues) { (current, _) in current }
+        urlComponents.setQueryItems(with: parameters)
+
+        let router = Router.getNowPlayingMovieList.get(params: "", httpHeaders: headers(), queryItems: urlComponents.queryItems)
+        let publisher: AnyPublisher<MovieListResponse, APIError> = context.doRequest(request: router)
+        return publisher
+    }
+    
+    func getPopularMovieList(page: Int) -> AnyPublisher<MovieListResponse, APIError> {
+        
+        let newKeyValues = [
+            "language": "en",
+            "page": String(page)
+        ]
+        
+        let parameters = defaultUrlParams.merging(newKeyValues) { (current, _) in current }
+        urlComponents.setQueryItems(with: parameters)
+
+        let router = Router.getPopularMovieList.get(params: "", httpHeaders: headers(), queryItems: urlComponents.queryItems)
         let publisher: AnyPublisher<MovieListResponse, APIError> = context.doRequest(request: router)
         return publisher
     }
