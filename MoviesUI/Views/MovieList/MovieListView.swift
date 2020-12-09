8b4280c99092b8b7e466665fb4e622886551b7ca
Obada <obada.semary@gmail.com>
2020-12-09 05:52:43 +0300

    Create MovieListView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesUI/Views/MovieList/MovieListView.swift
@@ -0,0 +1,46 @@
+//
+//  MovieListView.swift
+//  MoviesUI
+//
+//  Created by Abdelrahman Mohamed on 16.11.2020.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    
+    @ObservedObject var vm = MovieListVM(repo: Injector.mainServiceRepo)
+    @State private var isLoading = true
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVStack {
+                    ForEach(vm.topRatedMovies) { movie in
+                        MovieView(movie: movie)
+                            .onAppear {
+                                self.vm.loadMoreContentIfNeeded(currentItem: movie)
+                            }
+                    }
+                }
+            }
+            
+//            List(vm.topRatedMovies) { movie in
+//                MovieView(movie: movie)
+//                    .onAppear {
+//                        self.vm.loadMoreContentIfNeeded(currentItem: movie)
+//                    }
+//            }
+            .navigationBarTitle("Movies", displayMode: .large)
+        }
+        .onAppear {
+            self.vm.getTopRatedMovieList()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListView()
+    }
+}
