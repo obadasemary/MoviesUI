86a4c6a4e790899c857153f2a4c40564ae93fcb6
Obada <obada.semary@gmail.com>
2020-12-09 05:51:41 +0300

    Create MovieListResponse.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesUI/Network/Data Models/MovieListResponse/MovieListResponse.swift
@@ -0,0 +1,54 @@
+//
+//  MovieListResponse.swift
+//  MoviesUI
+//
+//  Created by Abdelrahman Mohamed on 23.11.2020.
+//
+
+import Foundation
+
+// MARK: - MovieListResponse
+struct MovieListResponse: Codable {
+    
+    let page, totalResults, totalPages: Int?
+    let results: [Movie]?
+
+    enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        case results
+    }
+}
+
+// MARK: - Result
+struct Movie: Codable, Identifiable {
+    
+    let popularity: Double?
+    let voteCount: Int?
+    let video: Bool?
+    let posterPath: String?
+    let id: Int
+    let adult: Bool?
+    let backdropPath, originalLanguage, originalTitle: String?
+    let genreIDS: [Int]?
+    let title: String?
+    let voteAverage: Double?
+    let overview, releaseDate: String?
+
+    enum CodingKeys: String, CodingKey {
+        case popularity
+        case voteCount = "vote_count"
+        case video
+        case posterPath = "poster_path"
+        case id, adult
+        case backdropPath = "backdrop_path"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case genreIDS = "genre_ids"
+        case title
+        case voteAverage = "vote_average"
+        case overview
+        case releaseDate = "release_date"
+    }
+}
