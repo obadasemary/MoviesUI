b9edcdc4447960e72df615ebb55d824e4838af2a
Obada <obada.semary@gmail.com>
2020-12-11 08:24:50 +0300

    Update MovieListView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesUI/Views/MovieList/MovieListView.swift
+++ MoviesUI/Views/MovieList/MovieListView.swift
@@ -10,36 +10,49 @@ import SwiftUI
 struct MovieListView: View {
     
     @ObservedObject var vm = MovieListVM(repo: Injector.mainServiceRepo)
-    @State private var isLoading = true
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVStack {
-                    ForEach(vm.topRatedMovies) { movie in
-                        MovieView(movie: movie)
-                            .onAppear {
-                                self.vm.loadMoreContentIfNeeded(currentItem: movie)
-                            }
+            
+            VStack {
+                CustomTabsView(currentTab: self.$vm.currentTab, tabs: [.topRated, .upcoming, .nowPlaying, .popular])
+                    .onReceive(self.vm.$currentTab, perform: { currentValue in
+                        
+                        switch currentValue {
+                        
+                        case .upcoming:
+                            self.vm.getUpcomingMovieList()
+                        case .nowPlaying:
+                            self.vm.getNowPlayingMovieList()
+                        case .popular:
+                            self.vm.getPopularMovieList()
+                        default:
+                            return
+                        }
+                    })
+                
+                ScrollView {
+                    VStack {
+                        
+                        switch self.vm.currentTab {
+                        
+                        case .topRated:
+                            MoviesView(movies: vm.topRatedMovies, currentTab: .topRated)
+                                .environmentObject(self.vm)
+                        case .upcoming:
+                            MoviesView(movies: vm.upcomingMovies, currentTab: .upcoming)
+                                .environmentObject(self.vm)
+                        case .nowPlaying:
+                            MoviesView(movies: vm.nowPlayingMovies, currentTab: .nowPlaying)
+                                .environmentObject(self.vm)
+                        case .popular:
+                            MoviesView(movies: vm.popularMovies, currentTab: .popular)
+                                .environmentObject(self.vm)
+                        }
                     }
                 }
             }
             .navigationBarTitle("Movies", displayMode: .large)
-            
-//            if vm.isLoadingPage {
-////                ProgressView()
-////                UIKitActivityIndicator(isAnimating: .constant(true), style: .medium)
-////                    .configure {
-////                        $0.color = UIColor.orange
-////                    }
-//                LoadingView()
-//            }
-            //            List(vm.topRatedMovies) { movie in
-            //                MovieView(movie: movie)
-            //                    .onAppear {
-            //                        self.vm.loadMoreContentIfNeeded(currentItem: movie)
-            //                    }
-            //            }
         }
         .onAppear {
             self.vm.getTopRatedMovieList()
