1aa24c49d03ca15e032a0cb38f755effad0701a7
Obada <obada.semary@gmail.com>
2020-12-16 21:06:36 +0300

    Update AuthorizationDataManager.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesUI/Network/AuthorizationDataManager.swift
+++ MoviesUI/Network/AuthorizationDataManager.swift
@@ -1,78 +1,47 @@
-////
-////  AuthorizationDataManager.swift
-////  MoviesUI
-////
-////  Created by Abdelrahman Mohamed on 18.11.2020.
-////  Copyright © 2020 Abdelrahman Mohamed. All rights reserved.
-////
 //
-//import Foundation
+//  AuthorizationDataManager.swift
+//  MoviesUI
 //
-//private let authModelKey = "authModelKey"
+//  Created by Abdelrahman Mohamed on 18.11.2020.
+//  Copyright © 2020 Abdelrahman Mohamed. All rights reserved.
 //
-//protocol AuthorizationDataManagerType {
-////    func saveAuthorizationModel(model: LoginRootModel)
-////    func getAuthorizationModel() -> LoginRootModel?
-////    func getBearerToken() -> String?
-////    func clearAuthorization()
-//}
-//
-//class AuthorizationDataManager: AuthorizationDataManagerType {
-//    
-//    static let shared = AuthorizationDataManager()
-//    
-////    var isLoggedIn: Bool {
-////        return getAuthorizationModel() != nil
-////    }
-//    
-////    func saveAuthorizationModel(model: LoginRootModel) {
-////        let playingItMyWay = model
-////        let userDefaults = UserDefaults.standard
-////        do {
-////            try userDefaults.setObject(playingItMyWay, forKey: authModelKey)
-////        } catch {
-////            print(error.localizedDescription)
-////        }
-////    }
-////
-////    func getAuthorizationModel() -> LoginRootModel? {
-////        let userDefaults = UserDefaults.standard
-////        do {
-////            let playingItMyWay = try userDefaults.getObject(forKey: authModelKey, castTo: LoginRootModel.self)
-////            print(playingItMyWay)
-////            return playingItMyWay
-////        } catch {
-////            print(error.localizedDescription)
-////            return nil
-////        }
-////    }
-//    
-////    func clearAuthorization() {
-////        let defaults = UserDefaults.standard
-////        defaults.removeObject(forKey: authModelKey)
-////    }
-////    
-////    func getBearerToken() -> String? {
-////        print(getAuthorizationModel()?.data?.credentials!.accessToken ?? "No Access token")
-////        if let token = getAuthorizationModel()?.data?.credentials!.accessToken {
-////            return token
-////        }
-////        return ""
-////    }
-////
-////    func getToken() -> String {
-////        guard let token = getAuthorizationModel()?.data?.credentials!.accessToken else {
-////            return ""
-////        }
-////        return token
-////    }
-////    
-////    func getEmail() -> String {
-////            
-////        guard let email = getAuthorizationModel()?.data?.customer?.email else {
-////            return ""
-////        }
-////        
-////        return email
-////    }
-//}
+
+import Foundation
+
+protocol AuthorizationDataManagerType {
+    func saveAuthorizationSession(sessionId: String)
+    func getAuthorizationSession() -> String?
+    func clearAuthorization()
+}
+
+class AuthorizationDataManager: AuthorizationDataManagerType {
+
+    static let shared = AuthorizationDataManager()
+    private let sessionIdKey = "sessionId"
+    
+    func saveAuthorizationSession(sessionId: String) {
+        let userDefaults = UserDefaults.standard
+        do {
+            try userDefaults.set(sessionId, forKey: sessionIdKey)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    func getAuthorizationSession() -> String? {
+        let userDefaults = UserDefaults.standard
+        do {
+            let sessionId = try userDefaults.object(forKey: sessionIdKey)
+            print(sessionId)
+            return sessionId as? String
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
+    func clearAuthorization() {
+        let defaults = UserDefaults.standard
+        defaults.removeObject(forKey: sessionIdKey)
+    }
+}
