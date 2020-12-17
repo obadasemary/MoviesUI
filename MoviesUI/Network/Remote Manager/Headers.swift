fb518f7629fe223c372ede82f062c5d84fc4aa7b
Obada <obada.semary@gmail.com>
2020-12-17 08:22:50 +0300

    Update Headers.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesUI/Network/Remote Manager/Headers.swift
+++ MoviesUI/Network/Remote Manager/Headers.swift
@@ -11,7 +11,7 @@ func headers() -> [String: String] {
     
     var headers = [String: String]()
     
-    headers["Content-Type"] = "application/json"
+    headers["Content-Type"] = "application/json;charset=utf-8"
     headers["Accept"] = "application/json"
     headers["X-OS-Name"] = "ios"
     headers["User-Agent"] = "iOSMobileApp"
