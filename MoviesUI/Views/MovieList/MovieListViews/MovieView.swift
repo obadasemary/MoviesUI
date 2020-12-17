4d4675f9b91431106a7d435bfb0dba8a1690e7cc
Obada <obada.semary@gmail.com>
2020-12-17 08:25:35 +0300

    Update MovieView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesUI/Views/MovieList/MovieListViews/MovieView.swift
+++ MoviesUI/Views/MovieList/MovieListViews/MovieView.swift
@@ -9,14 +9,20 @@ import SwiftUI
 
 struct MovieView: View {
     
+    @ObservedObject var addToListVM = AddToListVM(repo: Injector.mainServiceRepo)
     var movie: Movie
     
     var body: some View {
         VStack {
             
-            kfImageView(url: URLBuilder.imageUrl(path: movie.posterPath ?? "wwemzKWzjKYJFfCeiB57q3r4Bcm.png") ?? "")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
+            ZStack {
+                kfImageView(url: URLBuilder.imageUrl(path: movie.posterPath ?? "wwemzKWzjKYJFfCeiB57q3r4Bcm.png") ?? "")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                
+                UserScoreView(progress: CGFloat(movie.voteAverage ?? 0), boxSize: 70)
+                    .offset(x: UIScreen.main.bounds.width / 3.5 , y: 280)
+            }
             
             HStack {
                 VStack(alignment: .leading) {
@@ -36,26 +42,6 @@ struct MovieView: View {
             .padding([.horizontal, .top])
             
             Group {
-                
-//                HStack(spacing: 3) {
-//
-//                    RatingView(rating: Int(movie.voteAverage))
-//                        .cornerRadius(10)
-//                        .padding()
-//                        .overlay(
-//                            RoundedRectangle(cornerRadius: 10)
-//                                .stroke(Color.accentColor, lineWidth: 1)
-//                        )
-//
-//                    Spacer()
-//
-////                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-////                        Image(systemName: CustomBottonTabBarTabs.plus.rawValue)
-////                            .resizable()
-////                            .frame(width: 26, height: 26, alignment: .center)
-////                            .foregroundColor(.accentColor)
-////                    })
-//                }
             
                 RatingView(rating: Int(movie.voteAverage ?? 0))
                     .cornerRadius(10)
@@ -68,6 +54,9 @@ struct MovieView: View {
                 Text(movie.overview ?? "")
                     .font(.system(.body, design: .rounded))
                     .foregroundColor(.secondary)
+                
+                AddToListView(movieId: movie.id ?? 155, movieName: movie.title ?? "", doWeNeedSpacer: true)
+                    .environmentObject(self.addToListVM)
             }
             .padding()
         }
