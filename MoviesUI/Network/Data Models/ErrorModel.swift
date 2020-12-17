19e5e9678e6ed386f2dca688ac990ee6f0358813
Obada <obada.semary@gmail.com>
2020-12-17 08:22:32 +0300

    Create ErrorModel.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesUI/Network/Data Models/ErrorModel.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorModel.swift
+//  MoviesUI
+//
+//  Created by Abdelrahman Mohamed on 18.11.2020.
+//
+
+import Foundation
+
+struct ErrorModel: Decodable {
+    //var code: Int?
+    var message: String
+}
